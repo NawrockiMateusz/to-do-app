25e809cd93a0a76a6c8393f84d17d59ba8eefd95
Mateusz Nawrocki <mateusz.nawroo@gmail.com>
2023-02-06 22:35:54 +0100

    Created and added view for all elements

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.scss']
+  styleUrls: ['./todos.component.scss'],
 })
 export class TodosComponent implements OnInit {
+  taskArray = [
+    { name: 'zrobić pranie', isDone: false },
+    { name: 'zatankować samochód', isDone: true },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
